37acee3a631d9818be8edbfc11890e21d8997b2c
[Github <helgo1318@gmail.com>
2024-05-20 15:52:12 +0900

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 export default function App() {
+
+  const onClick = () => {
+
+    const onPlusNumber = () => {
+      return setNumber ()
+    }
+
+    const onMinusNumber = () => {
+      return 
+    }
+
+  }
+
+  const [plus, setPluse] = useState(0);
+  const [minus, setMinus] = useState(0);
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input type="number" /> 만큼을{" "}
+        <button onClick={() => onPlusNumber()}>더할게요</button>{" "}
+        <button onClick={() => onMinusNumber()}>뺄게요</button>
         <button>초기화</button>
       </div>
       <hr />
